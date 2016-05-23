3626e999f4a195f50f8cc5c0c4d0fe968281b3cf
romansysyuk <kobebryant41@gmail.com>
2016-05-23 22:30:57 +0300

    DB test

--- src/main/java/lviv/first/entity/Users.java
+++ src/main/java/lviv/first/entity/Users.java
@@ -1,6 +1,7 @@
 package lviv.first.entity;
 
 import javax.persistence.*;
+import java.util.List;
 
 /**
  * Created by kobeb on 23.05.2016.
@@ -13,11 +14,18 @@ public class Users {
     private long id;
 
 
-    @Column(length = 55, nullable = false,unique = true)
+    @Column(length = 55, nullable = false,unique = true,name = "username")
     private String username;
 
     @Column(length = 55, nullable = false)
     private String password;
+
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "users")
+    @JoinColumn(name = "username")
+    private UserInfo userInfo;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "users")
+    private List<Event> eventList;
 
     public Users() {
     }
@@ -27,5 +35,43 @@ public class Users {
         this.username = username;
     }
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public UserInfo getUserInfo() {
+        return userInfo;
+    }
+
+    public void setUserInfo(UserInfo userInfo) {
+        this.userInfo = userInfo;
+    }
+
+    public List<Event> getEventList() {
+        return eventList;
+    }
+
+    public void setEventList(List<Event> eventList) {
+        this.eventList = eventList;
+    }
 }
